8e26871fe98fd4eae9512642c705eeaa528f62e0
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2023-10-21 16:06:25 -0400

    Initial Commit

---
+++ wasteof.money/wasteof_moneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  wasteof_moneyApp.swift
+//  wasteof.money
+//
+//  Created by Oren Lindsey on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct wasteof_moneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
